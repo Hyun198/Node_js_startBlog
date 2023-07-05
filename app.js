efd0a754605144fa29dcdc909994483b14a84d80
hyun198 <hwn123h@naver.com>
2023-07-05 20:41:26 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,48 @@
+require('dotenv').config()
+const path = require('path')
+const express = require('express')
+const expressLayout = require('express-ejs-layouts')
+const methodOverride = require('method-override')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const MongoStore = require('connect-mongo')
+
+const connectDB = require('./server/config/db')
+const {isActiveRoute} = require('./server/helpers/routeHelpers')
+const session = require('express-session')
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(cookieParser())
+app.use(methodOverride('_method'))
+
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUnitailized: true,
+    store: MongoStore.create({
+        mongoUrl: process.env.DATABASE_URI
+    })
+
+}))
+
+app.use(express.static('public'))
+//Templateing engine
+app.use(expressLayout)
+app.set('layout', './layouts/template')
+app.set('view engine', 'ejs')
+
+
+app.use('/', require('./server/routes/main'))
+app.use('/', require('./server/routes/admin'))
+
+app.locals.isActiveRoute = isActiveRoute
+
+//connect DB
+connectDB();
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`server running on ${process.env.PORT}`)
+})
---
+++ server/routes/main.js
@@ -0,0 +1,89 @@
+const express= require('express')
+const router = express.Router()
+const Post = require('../models/Post')
+require('dotenv').config()
+
+/* GET /
+   HOME */
+router.get('', async (req,res)=>{
+    try{
+        const locals = {
+            title: "Nodejs blog",
+            desc: "Simple blog making",
+            name: "Hyun"
+        }
+        let perPage = 10;
+        let page = req.query.page || 1;
+
+        const data = await Post.aggregate([ {$sort: {createdAt: -1 } } ])
+        .skip(perPage * page -perPage)
+        .limit(perPage)
+        .exec();
+
+        const count = await Post.count();
+        const nextPage = parseInt(page) + 1;
+        const hasNextPage = nextPage <= Math.ceil(count/ perPage)
+
+        res.render('home', 
+        {locals, 
+            data,
+            current: page,
+            nextPage: hasNextPage ? nextPage: null,
+            currentRoute: '/'
+        });
+    }catch(err){
+        console.log(error);
+    }
+    
+    
+})
+
+router.get('', async (req,res)=>{
+    const locals = {
+        title: "Nodejs blog",
+        desc: "Simple blog making",
+        name: "Hyun"
+    }
+    try{
+        const data = await Post.find();
+        res.render('home', {locals, data});
+    }catch(err){
+        console.log(error);
+    }
+    
+    
+})
+
+
+router.get('/about', (req,res)=>{
+    res.render('about',{
+        currentRoute: '/about'
+    })
+})
+
+
+module.exports = router
+
+
+
+/* function insertPostData () {
+    Post.insertMany([
+        {
+            title: "Building AI",
+            desc:  "This is the body text",
+        },
+        {
+            title: "Building Node js",
+            desc:  "This is the body text",
+        },
+        {
+            title: "Building Flask",
+            desc:  "This is the body text",
+        },
+        {
+            title: "Building CGV",
+            desc:  "This is the body text",
+        },
+    ])
+}
+insertPostData();  */
